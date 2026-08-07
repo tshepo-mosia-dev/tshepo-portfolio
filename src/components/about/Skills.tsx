c73bd6fb8dc8eed297dcbb4d4570a9b9e5068ef3
Tshepo <tmosia216@gmail.com>
2026-08-07 20:06:21 +0200

    aaa

--- src/components/about/Skills.tsx
+++ src/components/about/Skills.tsx
@@ -10,7 +10,7 @@ const Skills = () => {
 
             </div>
 
-            <div className="grid lg:grid-cols-4 lg:items-start">
+            <div className="space-y-12 grid lg:grid-cols-4 lg:items-start">
                 {skillCategories.map((category) => (
                     <div key={category.title}>
                         <h3 className="mb-6 font-semibold">
